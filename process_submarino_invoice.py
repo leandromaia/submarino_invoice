f5415bb8ae1ff60b0008095957cf00fc947f89f4
zmailea <leandroc@inatel.br>
2019-05-27 22:42:42 -0300

    Get the raw values add to a dict object

--- process_submarino_invoice.py
+++ process_submarino_invoice.py
@@ -9,9 +9,27 @@ list_line_objects = list()
 
 for line in reader.readlines():
     total_values = len(line.split())
+    date = line.split()[0]
+    value = line.split()[total_values-2:total_values-1][0]
+    raw_description = line.replace(date, "").replace(value, "")
+
     obj_line = {
-        "date": line.split()[0],
-        "description":
-        "value": line.split()[total_values-2:total_values-1]
+        "date": date,
+        "description": raw_description,
+        "value": value
     }
-    import pdb; pdb.set_trace()
+    list_line_objects.append(obj_line)
+
+reader.close()
+
+file_csv_path = 'final_file.csv'
+
+writer = open(file_csv_path, 'w')
+
+for obj in list_line_objects:
+    writer.write("{0};{1};{2}".format(
+                                    obj.get('date'),
+                                    obj.get('description'),
+                                    obj.get('value')))
+
+writer.close()
