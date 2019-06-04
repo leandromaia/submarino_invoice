2d694527574add6c59ad1217cda3760e0b15ac8c
zmailea <leandroc@inatel.br>
2019-06-03 23:00:11 -0300

    Refactor to encapsulate read file into a function

--- process_submarino_invoice.py
+++ process_submarino_invoice.py
@@ -1,36 +1,46 @@
 #!/usr/bin/python3
-print("-----------Read Submarino Invoice Data------------\n")
 
-path = input("Type the raw file path: ")
+def request_input_file_path():
+    return input("Type the raw file path: ")
 
-reader = open(path, 'r')
+def read_file_lines():
+    try:
+        input_path = request_input_file_path()
+        with open(input_path, 'r') as reader:
+            return reader.readlines()
+    except OSError as e:
+        print("Error during read input file. Message: {0}".format(e)) 
 
-list_line_objects = list()
+def start():
+    print("-----------Read Submarino Invoice Data------------\n")
+    
+    list_line_objects = list()
 
-for line in reader.readlines():
-    total_values = len(line.split())
-    date = line.split()[0]
-    value = line.split()[total_values-2:total_values-1][0]
-    raw_description = line.replace(date, "").replace(value, "")
-    raw_description = raw_description.strip('D\n').strip()
+    for line in read_file_lines():
+        total_values = len(line.split())
+        date = line.split()[0]
+        value = line.split()[total_values-2:total_values-1][0]
+        raw_description = line.replace(date, "").replace(value, "")
+        raw_description = raw_description.strip('D\n').strip()
 
-    obj_line = {
-        "date": date,
-        "description": raw_description,
-        "value": value
-    }
-    list_line_objects.append(obj_line)
+        obj_line = {
+            "date": date,
+            "description": raw_description,
+            "value": value
+        }
+        list_line_objects.append(obj_line)
 
-reader.close()
+    file_csv_path = 'final_file.csv'
 
-file_csv_path = 'final_file.csv'
+    writer = open(file_csv_path, 'w')
 
-writer = open(file_csv_path, 'w')
+    for obj in list_line_objects:
+        writer.writelines("{0};{1};{2}\n".format(
+                                        obj.get('date'),
+                                        obj.get('description'),
+                                        obj.get('value')))
 
-for obj in list_line_objects:
-    writer.writelines("{0};{1};{2}\n".format(
-                                    obj.get('date'),
-                                    obj.get('description'),
-                                    obj.get('value')))
+    writer.close()
 
-writer.close()
+if __name__ == '__main__':
+    start()
